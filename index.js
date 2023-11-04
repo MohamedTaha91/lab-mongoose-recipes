7ae7977a95d274db9ec8d1a34a8c18390260f930
Mohamed Taha <Taha_Acme@hotmail.com>
2023-11-04 13:26:45 +0100

    lab done

--- index.js
+++ index.js
@@ -11,12 +11,38 @@ const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+    console.log(`Hello: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
+    const recipeOne = {
+      "title": "Curry Chicken",
+      "level": "Easy Peasy",
+      "ingredients": [
+        "Two chicken breasts",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt ,pepper and curry",
+        
+      ],
+      "cuisine": "Asian Fusion",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "Chef Taha"
+    }
     // Run your code here, after you have insured that the connection was made
+
+  return Recipe.create(recipeOne)
+  })
+  .then((addedRecipeOne)=>{
+    console.log(addedRecipeOne)
+    return Recipe.insertMany(data)
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
